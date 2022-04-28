5467227ae5d9ffd41c0154d80e5f15d397120b6e
devrndx <dev@round-x.com>
2022-04-29 00:58:45 +0900

    update prod url

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -71,7 +71,7 @@ def popupTokenTransferAll():
 @blueprint.route('/popupTokenRevertAll')
 def popupTokenRevertAll():
     isDebug = config('DEBUG', default=True, cast=bool)
-    API_BASE_URL = 'https://app.dev.rndx-wallet.io' if isDebug  else 'https://app.rndx-wallet.io'
+    API_BASE_URL = 'https://app.rndx-wallet.io' if isDebug  else 'https://app.rndx-wallet.io'
     return render_template("popup/popup-token-revertall.html", segment=popupTokenRevertAll, api_base_url=API_BASE_URL)
 
 @blueprint.route('/popupLockWalletAll')
